40e3f0d96f98158afac9cd8f4dafccbf431ae0e6
WangGuanru <frycrompton@gmail.com>
2022-09-07 23:04:16 +0800

    add a java file

---
+++ HongxinFSApple.java
@@ -0,0 +1,31 @@
+class Employee{
+	String name;
+	String id="abc";
+	private String team;
+	void Employee(String a,String b,String c){
+		name=a;
+		id=b;
+		team=c;
+	}
+	String getId(){
+		return id;
+	}
+
+}
+class test extends Employee{
+	String id="123";
+
+	String getId(){
+		return id+id;
+	}
+	public static void main(String[] args){
+		test a=new test();
+		System.out.println(a.getId());
+		int k,sum=0;
+		int q=-12;
+		for (k=0;k<=12;k=k+2){
+			sum=sum+k;
+		}
+		System.out.println(q%2-5);
+	}
+}
